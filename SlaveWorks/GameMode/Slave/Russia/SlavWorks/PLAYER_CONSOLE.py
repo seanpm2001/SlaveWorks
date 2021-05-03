0fe816000b80be232e011e0fa6ed735e0cd21b60
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-03 12:25:01 -0700

    PLAYER_CONSOLE.py
    The player console for the Slavic part of the game.

---
+++ SlaveWorks/GameMode/Slave/Russia/SlavWorks/PLAYER_CONSOLE.py
@@ -0,0 +1,33 @@
+# Start of script
+
+# Slavworks
+# No e for you, slav
+
+# Slavworks is the expansion pack of this game that aims towards slavery in Slavic countries, such as Russia, Belarus, Ukrainine, Serbia, Macedonia, etc.
+# I intend to have a transition to this stage similar to Portal 2 transferring from Aperture Labs to Wheatley labs
+print ("Slavworks gamemode is loading")
+playerConsole = str(input("Do you want to live a life as a Slavic slave? (y/N)"))
+slslaYN == str(playerConsole.upper())
+if (slslaYN == "Y"):
+  print ("What country do you want to start out as a slave in?")
+  print ("Russia (1800s)")
+  print ("Ukraine (1800s)")
+  print ("Serbia (1800s)")
+  print ("Belarusia (1800s)")
+  print ("Macedonia (1800s)")
+  print ("More options coming soon")
+  playerConsole = str(input("Where do you prefer?"))
+  if (playerConsole == "NaN"):
+    print ("This segment is not yet complete")
+  else:
+    print ("This segment is not yet complete")
+else:
+  print ("This segment is not yet complete")
+noMore = input("This area is incomplete. Press the [ENTER] key to quit")
+print ("Leaving slavworks")
+""" File info
+File type: Python 3.7 source file (*.py)
+File version: 1 (Monday, May 3rd 2021 at 12:25 pm)
+Line count (including blank lines and compiler line): 34
+"""
+# End of script
